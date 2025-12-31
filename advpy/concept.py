29dd2938102f6a0a0133b3c86b28480e4dcee9ba
SahithiVutukuri <vutukuri.sahithi27@gmail.com>
2025-12-31 10:13:41 +0530

    Add files via upload

---
+++ advpy/concept.py
@@ -0,0 +1,32 @@
+"""Mini Project
+
+GUI CALCULATOR APPLICATION
+A Tkinter -based Desktop Application
+
+What is Tkinter?
+Tkinter is python's buint-in library for creating Graphical user interface(GUI) applications.
+It provides widgets like window , button, entry, label, etc.
+It follows an event-driven programming model (code runs when a user clicks a button).
+  Purpose of this event 
+  Creates a calculator GUI
+  Allow user to:
+  enter numbers and operators
+  perform arithmetic calculations
+  clear the input
+  
+Uses pythons's eval() function to eveluate mathematical expressions.
+
+Key concepts used:
+ Concept                       Description
+ Tkinter widgets             Tk, Entry, Button
+ Event handling              Button click actions
+ Lambda functions            Passing button values
+ Grid layout                 positioning UI elements
+ Exception handling          Handling invalid expressions    """
+ 
+ 
+ 
+ 
+ 
+
+
